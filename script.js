4fb22f9ccc02ab627dd6a91d4358d7368d78d33e
Nilesh Patil <nileshyograjp335@gmail.com>
2026-03-20 17:42:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-// Typing
-const words=["Web Developer","SEO Expert","WordPress"];
-let i=0,j=0,isDeleting=false;
+const words = ["Web Developer", "SEO Specialist", "WordPress Expert"];
+let i = 0, j = 0, isDeleting = false;
 
-function type(){
-let text=words[i];
-document.getElementById("typing").textContent=text.substring(0,j);
+function type() {
+  let text = words[i];
+  document.getElementById("typing").textContent = text.substring(0, j);
 
-if(!isDeleting && j<text.length) j++;
-else if(isDeleting && j>0) j--;
+  if (!isDeleting && j < text.length) j++;
+  else if (isDeleting && j > 0) j--;
 
-if(j===text.length) isDeleting=true;
-if(j===0){isDeleting=false;i=(i+1)%words.length;}
+  if (j === text.length) isDeleting = true;
+  if (j === 0) { isDeleting = false; i = (i + 1) % words.length; }
 
-setTimeout(type,100);
+  setTimeout(type, 100);
 }
 type();
